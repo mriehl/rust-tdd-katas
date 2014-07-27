2193901d9ea9c5af59373077e50e1ce831b3ce7e
mriehl <max@riehl.io>
2014-07-27 16:00:10 +0200

    ported some tests to use the shiny framework

--- rover/src/rover_test.rs
+++ rover/src/rover_test.rs
@@ -1,69 +1,60 @@
 use rover;
 use coords::Position2D;
 
-#[test]
-fn should_create_new_rover(){
-    let r = rover::new();
-    assert_eq!(r.position, Position2D{x: 0, y: 0})
-}
+describe!(
+    it "should create new rover" {
+        let r = rover::new();
+        assert_eq!(r.position, Position2D{x: 0, y: 0})
+    }
 
-#[test]
-fn should_move_north(){
-    let mut r = rover::new();
-    r.move(rover::YDelta(1));
-    assert_eq!(r.position, Position2D{x: 0, y: 1})
-}
+    it "should move north" {
+        let mut r = rover::new();
+        r.move(rover::YDelta(1));
+        assert_eq!(r.position, Position2D{x: 0, y: 1})
+    }
 
-#[test]
-fn should_move_north_twice(){
-    let mut r = rover::new();
-    r.move(rover::YDelta(2));
-    assert_eq!(r.position, Position2D{x: 0, y: 2})
-}
+    it "should move north twice" {
+        let mut r = rover::new();
+        r.move(rover::YDelta(2));
+        assert_eq!(r.position, Position2D{x: 0, y: 2})
+    }
 
+    it "should move east" {
+        let mut r = rover::new();
+        r.move(rover::XDelta(1));
+        assert_eq!(r.position, Position2D{x: 1, y: 0})
+    }
 
-#[test]
-fn should_move_east(){
-    let mut r = rover::new();
-    r.move(rover::XDelta(1));
-    assert_eq!(r.position, Position2D{x: 1, y: 0})
-}
+    it "should move east twice" {
+        let mut r = rover::new();
+        r.move(rover::XDelta(2));
+        assert_eq!(r.position, Position2D{x: 2, y: 0})
+    }
 
-#[test]
-fn should_move_east_twice(){
-    let mut r = rover::new();
-    r.move(rover::XDelta(2));
-    assert_eq!(r.position, Position2D{x: 2, y: 0})
-}
+    it "should move south" {
+        let mut r = rover::new();
+        r.move(rover::YDelta(-1));
+        assert_eq!(r.position, Position2D{x: 0, y: -1})
+    }
 
-#[test]
-fn should_move_south(){
-    let mut r = rover::new();
-    r.move(rover::YDelta(-1));
-    assert_eq!(r.position, Position2D{x: 0, y: -1})
-}
+    it "should move south twice" {
+        let mut r = rover::new();
+        r.move(rover::YDelta(-2));
+        assert_eq!(r.position, Position2D{x: 0, y: -2})
+    }
 
-#[test]
-fn should_move_south_twice(){
-    let mut r = rover::new();
-    r.move(rover::YDelta(-2));
-    assert_eq!(r.position, Position2D{x: 0, y: -2})
-}
+    it "should move west" {
+        let mut r = rover::new();
+        r.move(rover::XDelta(-1));
+        assert_eq!(r.position, Position2D{x: -1, y: 0})
+    }
 
-
-#[test]
-fn should_move_west(){
-    let mut r = rover::new();
-    r.move(rover::XDelta(-1));
-    assert_eq!(r.position, Position2D{x: -1, y: 0})
-}
-
-#[test]
-fn should_move_west_twice(){
-    let mut r = rover::new();
-    r.move(rover::XDelta(-2));
-    assert_eq!(r.position, Position2D{x: -2, y: 0})
-}
+    it "should move west twice" {
+        let mut r = rover::new();
+        r.move(rover::XDelta(-2));
+        assert_eq!(r.position, Position2D{x: -2, y: 0})
+    }
+  )
 
 #[test]
 fn should_move_by_vector(){
