55ea6f6984e90f38e87dd7ae975d0e99d6d8129b
mriehl <max@riehl.io>
2014-11-26 12:37:58 +0100

    dereference pointers before modulo operation

--- fizzbuzz/src/fizzlib.rs
+++ fizzbuzz/src/fizzlib.rs
@@ -1,13 +1,13 @@
 use std::string::String;
 
 pub fn fizzbuzz(number: &int) -> String{
-    if number % 15 == 0 {
+    if *number % 15 == 0 {
         String::from_str("fizzbuzz")
     }
-    else if number % 3 == 0 {
+    else if *number % 3 == 0 {
         String::from_str("fizz")
     }
-    else if number % 5 == 0 {
+    else if *number % 5 == 0 {
         String::from_str("buzz")
     }
     else {
